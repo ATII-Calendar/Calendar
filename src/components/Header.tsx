6f40801fe097d90f0cca43e47d260c3015798edd
anott03 <amitav_nott@ryecountryday.org>
2021-02-26 18:01:01 -0500

    working on the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useUserValue } from '../contexts/userContext';
 import { actionTypes } from '../reducer';
 import { auth } from '../services/firebase/firebaseConfig';
 import '../styles/Header.css';
 import firebase from 'firebase';
+import { Link } from 'react-router-dom';
+import { Button, Dropdown, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 export default function Header(): JSX.Element {
 
@@ -13,7 +15,7 @@ export default function Header(): JSX.Element {
   if (userState.user) {
     user = userState.user;
   }
-
+  let [showMenu, setShowMenu] = useState(false);
   const dispatch = useUserValue().dispatch;
   const history = useHistory()
 
@@ -24,20 +26,63 @@ export default function Header(): JSX.Element {
     })
   }
 
+  /*
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/home">RCDS Calendar</a>
+        <Link className="navbar-brand" to="/home">RCDS Calendar</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
+
         { user &&
-          <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
-            <p style={{margin: '5px'}}>{user.displayName}</p>
-            <button className="btn btn-danger" onClick={signOut}>Sign Out</button>
+          <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+            <button className="btn btn-link" style={{color: 'white', textDecoration: 'none'}} onClick={() => setShowMenu(!showMenu)}>{user.displayName}</button>
+            { showMenu && (
+              <button className="btn btn-danger btn-small btn-rounded" onClick={signOut}>Sign Out</button>
+            )}
           </div>
         }
       </div>
     </nav>
+  )
+   */
+
+  let brandStyles: React.CSSProperties = {
+    color: 'white',
+    textDecoration: 'none',
+    padding: '10px'
+  }
+
+  let listLinkStyles: React.CSSProperties = {
+    textDecoration: 'none'
+  }
+
+  return (
+    <Navbar variant="dark" bg="dark" expand="lg" className="d-flex">
+      <Navbar.Brand><Link to="/" style={brandStyles}>RCDS Calendar</Link></Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbar-nav" />
+      <Navbar.Collapse id="navbar-nav">
+        <Nav className="mr-auto">
+        </Nav>
+
+        { user &&
+          <Dropdown className="ml-auto">
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              {user.displayName}
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item>
+                <Link to="/settings" style={listLinkStyles}>Settings</Link>
+              </Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item onClick={signOut} style={{color: 'red'}}>Sign Out</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        }
+
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
