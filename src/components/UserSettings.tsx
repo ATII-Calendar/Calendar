0fe9304022e982b065dd592d0e1dbb458a8b9183
Amitav Nott <amitav_nott@ryecountryday.org>
2021-03-13 18:33:56 -0500

    [wip] settings page

--- src/components/UserSettings.tsx
+++ src/components/UserSettings.tsx
@@ -4,16 +4,39 @@ import { useUserValue } from '../contexts/userContext'
 import Header from './Header';
 
 export default function UserSettings() {
-  let { state, dispatch } = useUserValue();
-  let { user, classes } = state;
+  let { state } = useUserValue();
+  let { classes } = state;
+  const blocks = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
 
   console.log(classes)
   return (
+    <>
+    <Header />
     <div className="user-settings">
-      <Header />
-      <h1>General</h1>
+      <div className="user-settings-sidebar">
+        <h3>General</h3>
+        <h3>Schedule</h3>
+      </div>
 
-      <h1>Schedule</h1>
+      <div className="user-settings-body">
+        <h1>General</h1>
+        <h1>Schedule</h1>
+        <div className="schedule-items">
+          <form>
+          {classes.map((c: string, i: number) => {
+            console.log(c)
+            return (
+              <div className="schedule-item" key={i}>
+                <h4>{blocks[i]}</h4>
+                <input type="text" value={c ? c : ""} />
+              </div>
+            )
+          })}
+          <button type="submit" className="btn btn-primary">Update Schedule</button>
+          </form>
+        </div>
+      </div>
     </div>
+    </>
   )
 }
