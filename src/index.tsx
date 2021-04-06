b46b51281baf0b598c1280a5648d96f7fac4898b
anott03 <amitav_nott@ryecountryday.org>
2021-04-06 10:19:36 -0400

    Unnecessary import in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import Adminui from './components/adminui';
 
 ReactDOM.render(
   <React.StrictMode>
