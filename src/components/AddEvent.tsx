d9477238f844cecff17398d9fbad35035b94eaee
anott03 <amitav_nott@ryecountryday.org>
2021-04-21 08:27:27 -0400

    working on adding events
    getting the values from the input fields

--- src/components/AddEvent.tsx
+++ src/components/AddEvent.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-
 import Button from '@material-ui/core/Button';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
@@ -15,43 +14,46 @@ export interface SimpleDialogProps {
 }
 
 export function AddEventDialog({ onClose, selectedValue, open }: any) {
+  let [title, setTitle] = useState("");
+  let [description, setDescription] = useState("");
+  let [startDate, setStartDate] = useState(null);
+  let [endDate, setEndDate] = useState(null);
+
   const handleClose = () => {
     onClose(selectedValue);
   };
 
   const addEvent = (e: any) => {
-    let title = e.target.title.value;
-    let description = e.target.description.value;
-    let startDate = new Date(e.target.startDate.value);
-    let endDate = new Date(e.target.endDate.value);
-    addUserEvent("", title, startDate, endDate ? endDate : null, endDate ? true : false);
+    console.log(title);
+    console.log(description);
+    console.log(startDate);
+    console.log(endDate);
+    handleClose();
     e.preventDefault();
   }
 
   return (
     <Dialog open={open}>
       <DialogTitle id="simple-dialog-title">Add Event</DialogTitle>
-      <form onSubmit={addEvent}>
       <DialogContent>
         <Typography variant="subtitle1">Title</Typography>
-        <input id="title" style={{width: '100%', padding: '10px', border: "solid 1px black"}}/>
+        <input id="title" style={{width: '100%', padding: '10px', border: "solid 1px black"}} onChange={(e: any) => {setTitle(e.target.value)}}/>
         <Typography variant="subtitle1">Description</Typography>
-        <textarea id="description" style={{width: '100%', padding: '10px', border: "solid 1px black"}}/>
+        <textarea id="description" style={{width: '100%', padding: '10px', border: "solid 1px black"}} onChange={(e: any) => {setDescription(e.target.value)}}/>
 
         <input id="alldayCheckbox" type="checkbox"/>
         <label style={{marginLeft: "10px"}} htmlFor="alldayCheckbox">All Day</label>
 
         <Typography variant="subtitle1">Start Date</Typography>
-        <input id="startDate" type="date" style={{width: '100%', padding: '10px', border: "solid 1px black"}}/>
+        <input id="startDate" type="date" style={{width: '100%', padding: '10px', border: "solid 1px black"}} onChange={(e: any) => {setStartDate(new Date(e.target.value))}}/>
         <Typography variant="subtitle1">End Date</Typography>
-        <input id="endDate" type="date" style={{width: '100%', padding: '10px', border: "solid 1px black"}}/>
+        <input id="endDate" type="date" style={{width: '100%', padding: '10px', border: "solid 1px black"}} onChange={(e: any) => {setStartDate(new Date(e.target.value))}}/>
 
         <DialogActions>
           <Button color="secondary" onClick={handleClose}>Cancel</Button>
-          <Button color="primary" type="submit">Done</Button>
+          <Button color="primary" onClick={addEvent}>Done</Button>
         </DialogActions>
       </DialogContent>
-      </form>
     </Dialog>
   );
 }
