83e62bd125ebe7fe656173bc55d057d36503fe55
Allenander <58124561+Allenander@users.noreply.github.com>
2021-05-10 09:54:13 -0400

    Update adminui.jsx

--- src/components/adminui.jsx
+++ src/components/adminui.jsx
@@ -1,3 +1,4 @@
+import { getSectionClassNames } from '@fullcalendar/react';
 import { Button } from '@material-ui/core';
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
@@ -6,35 +7,45 @@ export default function Adminui() {
 
   let day1 = new Date(2022, 9, 5)
   let history = useHistory();
+  let offDays  = [Date(2022, 7, 4)];
+  let [initialDate, setInitialDate] = React.useState('');
+  let [list, setList] = React.useState(offDays);
+  let [date, setDate] = React.useState('');
 
-  // to be implemented, to add inputted off days into a list.
-  function offDays() 
+  function handleChange(event) 
   {
-
+    setDate(event.target.value);
   }
 
-  function buttonClick()
+  function handleAdd()
   {
-    
+    let newList = list.concat(date);
+
+    setList(newList);
   }
 
-  function buttonClick3()
+  function handleChange2(event) 
   {
-    history.push("/hello");
+    setInitialDate(event.target.value);
+  }
+
+  function handleAdd2()
+  {
+    day1 = initialDate;
   }
 
   // fix onChange for each I think
   return (
    <div>
      <span> <h1> Hello admin! </h1> </span>
-     <input style={{fontSize: 25, padding: '10px'}} type="date" placeholder="Start Date" onChange={item => day1}/>
-     <Button color="primary" variant="contained" style={{marginLeft: '0', height: 50, width: 250, textAlign:'center', float: 'initial'}} onClick={() => buttonClick()}> 
+     <input style={{fontSize: 25, padding: '10px'}} type="date" placeholder="Start Date" onChange={handleChange2}/>
+     <Button color="primary" variant="contained" style={{marginLeft: '0', height: 50, width: 250, textAlign:'center', float: 'initial'}} onChange={handleAdd2}> 
       Set day 1
       </Button>
       <div>
       </div>
-      <input style={{fontSize: 25, padding: '10px'}} type="date" placeholder="Off day" onChange={item => day1}/>
-      <Button color="primary" variant="contained" style={{marginLeft: '0%', height: 50, width: 250, textAlign:'center', float: 'initial'}} onClick={() => buttonClick3()}> 
+      <input style={{fontSize: 25, padding: '10px'}} type="date" placeholder="Off day" onChange={handleChange}/>
+      <Button color="primary" variant="contained" style={{marginLeft: '0%', height: 50, width: 250, textAlign:'center', float: 'initial'}} onChange={handleAdd}> 
       Add days off
       </Button>
    </div>
