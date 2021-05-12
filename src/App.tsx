5f5c13c1eb0b5a1f746ab529842e292a613f1a9e
anott03 <amitav_nott@ryecountryday.org>
2021-05-12 19:45:29 -0400

    commenting app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,27 +11,37 @@ import UserSettings from './components/UserSettings';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 
+// this is how we can customize the theme
+// changes to things like colors will automatically apply to components using
+// the theme
 const theme = createMuiTheme({
   palette: {
     secondary: {
-      main: '#fed457',
+      main: '#fed457', // overriding the default secondary color to be rcds gold
     },
   },
 });
 
 function App() {
+  // global state
   let user: any;
   let userState = useUserValue().state;
   if (userState) {
     user = userState.user;
   }
 
+  // routes are fairly self-explanitory
+  // `path` is the relative path following the URL, and `render` is a function
+  // that returns JSX, which in our case will be the component/page
   return (
     <ThemeProvider theme={theme}>
     <BrowserRouter>
       <Route exact path="/">
-        { user ? <Redirect to="/welcome"/>
-        : <Redirect to="/signin"/> }
+        { // go to /welcome if the user is logged in,
+          // otherwise go to /signin
+          user ? <Redirect to="/welcome"/>
+          : <Redirect to="/signin"/>
+        }
       </Route>
       <Route exact path="/welcome" render={() => <WelcomePage/>}/>
       <Route exact path="/home" render={() => <Home/>}/>
